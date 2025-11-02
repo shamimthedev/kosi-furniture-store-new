f71ffb7dfa6f23718e61da3527932cd4c7af096f
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-11-02 10:20:49 +0600

    "feat: update homepage with proper file import"

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-import Home from '@/components/pages/Home'
+import Header from "@/components/layouts/Header"
+import Hero from "@/components/sections/Hero"
+import TopPicks from "@/components/sections/TopPicks"
+import NewArrival from "@/components/sections/NewArrival"
+import Feature from "@/components/sections/Feature"
+import ImageGallery from "@/components/sections/ImageGallery"
+import Footer from "@/components/layouts/Footer"
 
-export default function Page() {
-  return <Home />
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <TopPicks />
+        <NewArrival />
+        <Feature />
+        <ImageGallery />
+      </main>
+      <Footer />
+    </>
+  )
 }
